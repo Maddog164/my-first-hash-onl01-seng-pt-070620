3930977d07b8a5dd72c0eb61020709a5923e6c9c
Denise Henderson <denise@henderson3.us>
2020-07-16 01:10:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,7 +8,7 @@ end
 
 def shipping_manifest
   shipping_manifest = {"whale bone corsets" => 5,
-                       "porcelain vases" => 2,
+                       "porcelain vases" => 2, 
                        "oil paintings" => 3}
 end
 
@@ -18,10 +18,10 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
+  return shipping_manifest["oil paintings"]
+end
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
-end
 
 def adding
   shipping_manifest = {
@@ -40,3 +40,5 @@ end
 
 
   # return the shipping_manifest hash below
+
+
